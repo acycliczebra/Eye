02b3e80c7bd9a91164d6aab4f2b9a053a53cd172
Atta Zadeh <atta.h.zadeh@gmail.com>
2018-03-24 16:55:53 -0400

    made the interpreter sensible

--- eye/runtime/interpreter/interpreter.py
+++ eye/runtime/interpreter/interpreter.py
@@ -4,6 +4,26 @@ import json
 class ExecutionError(ValueError):
     pass
 
+class Value:
+    def show(self):
+        pass
+    def call(self, parameters, symbol_table):
+        pass
+
+
+class SymbolTable:
+    def __init__(self, table={}):
+        self.builtin = {}
+        self.table = table
+
+    def add(self, name, value):
+        self.table[name] = value
+
+    def type_of(self, name):
+        pass
+
+    def get(self, name):
+        pass
 
 class ASTObject:
     def visit(self, symbol_table):
@@ -12,56 +32,13 @@ class ASTObject:
 
 # Statements
 
-class DeclarationStatement(ASTObject):
-    def __init__(self, obj):
-        self.name = obj['name']
-        self.value = ast_creator(obj['value'])
-
-    def visit(self, symbol_table):
-        symbol_table = super().visit(symbol_table)
-
-        return {
-            **symbol_table,
-            self.name: self.value
-        }
-
-class FunctionCallExpression(ASTObject):
-    def __init__(self, obj):
-
-        if isinstance(obj['function'], str):
-            self.function = Id({
-                'value': obj['function']
-            })
-        else:
-            self.function = ast_creator(obj['function'])
-        self.parameters = [ast_creator(x) for x in obj['parameters']]
-
-    def visit(self, symbol_table):
-        symbol_table = super().visit(symbol_table)
-
-        if isinstance(self.function, Id): #TODO: better way of doing this, I'm not seeing the patter yet
-            if self.function.value == 'print':
-                for param in self.parameters:
-                    if isinstance(param, String):
-                        print(param.value, end='')
-                print('')
-
-            else:
-                f = symbol_table[self.function.value]
-                stack = {
-                    **symbol_table,
-                }
-                #TODO: check if arg and parameters have the same length
-                for arg, param in zip(f.args, self.parameters):
-                    stack[arg] = param
-
-                f.visit(stack) #TODO: we are sending in the symbol_table, this is like python. We don't want it like that, or do we?
-                #TODO: handle return statment
-        return {**symbol_table}
-
 # Expressions
 
-class LambdaExpression(ASTObject):
+class Expression(ASTObject):
+    def value(self):
+        raise NotImplementedError('abstract method')
+
+class LambdaExpression(Expression):
     def __init__(self, obj):
         self.args = obj['args']
         self.statements = [ast_creator(x) for x in obj['statements']]
@@ -69,9 +46,23 @@ class LambdaExpression(ASTObject):
     def visit(self, symbol_table):
         symbol_table = super().visit(symbol_table)
 
-        for statment in self.statements:
-            symbol_table = statment.visit(symbol_table)
         return {**symbol_table}
+
+    def value(self):
+        class Function(Value):
+            def __init__(self, args, statements):
+                self.args = args
+                self.statements = statements
+            def call(self, parameters, symbol_table):
+                #TODO: check if arg and parameters have the same length
+                stack = {**symbol_table}
+                for arg, param in zip(self.args, parameters):
+                    stack[arg] = param
+
+                for statement in self.statements:
+                    stack = statement.visit(stack)
+
+        return Function(self.args, self.statements)
 
 # Atoms
 
@@ -88,6 +79,64 @@ class String(ASTObject):
 
     def visit(self, symbol_table):
         return {**symbol_table}
+
+
+class DeclarationStatement(ASTObject):
+    def __init__(self, obj):
+        self.name = obj['name']
+        self.value = ast_creator(obj['value']).value()
+
+    def visit(self, symbol_table):
+        symbol_table = super().visit(symbol_table)
+
+        print('value: ', self.value)
+
+        return {
+            **symbol_table,
+            self.name: self.value
+        }
+
+def print_function(parameters):
+    for param in parameters:
+        if isinstance(param, String):
+            print(param.value, end='')
+    print('')
+
+class PrintFunction(Value):
+    def call(self, parameters, symbol_table):
+        for param in parameters:
+            if isinstance(param, String):
+                print(param.value, end='')
+        print('')
+
+
+class FunctionCallExpression(ASTObject):
+    def __init__(self, obj):
+
+        if isinstance(obj['function'], str):
+            self.function = Id({
+                'value': obj['function']
+            })
+        else:
+            self.function = ast_creator(obj['function'])
+
+        self.parameters = [ast_creator(x) for x in obj['parameters']]
+
+    def visit(self, symbol_table):
+        symbol_table = super().visit(symbol_table)
+
+        if self.function.value in symbol_table:
+            stack = {
+                **symbol_table,
+            }
+            f = symbol_table[self.function.value]
+            print('ISINSTANCE OF VALUE: ', isinstance(f, Value))
+            f.call(self.parameters, stack) #TODO: we are sending in the symbol_table, this is like python where the values are based on what is defined alread. We don't want it like that, or do we?
+            #TODO: handle return statment
+        else:
+            ValueError('function not found')
+        return {**symbol_table}
+
 
 # Top Level
 
@@ -138,6 +187,10 @@ def interpret(ast):
         raise ExecutionError('File requires version "0.0.1"')
 
     top_level = TopLevel(ast)
-    top_level.visit({})
+    global_symbols = {
+        'print': PrintFunction(),
+    }
+
+    top_level.visit(global_symbols)
 
     return 0
